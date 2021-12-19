fd3e4aa568b4b8369d298b3a42c8ccc85978c1bc
Karen Olson <karmarie@gmail.com>
2021-12-19 16:43:47 -0600

    Add error handling for invalid command

--- todo.js
+++ todo.js
@@ -5,6 +5,8 @@ console.log(process.argv);
 // Set args to a string array of command line arguments
 const args = process.argv;
 
+const commands = ["new", "get", "complete", "help"];
+
 function usage() {
   const usageText = `
     Todo helps you manage your todo tasks.
@@ -30,5 +32,11 @@ function errorLog(error) {
 // Make sure there are only 3 arguments
 if (args.length > 3) {
   errorLog("Please enter just one argument");
+  usage();
+}
+
+// Make sure the user entered a valid command word
+if (!commands.includes(args[2])) {
+  errorLog("Please enter a valid command");
   usage();
 }
